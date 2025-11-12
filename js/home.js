e9061b43fe628afd9487e54906c5940fcac4e86b
Laura <lauraof@al.insper.edu.br>
2025-11-12 13:27:54 -0300

    javascript funcionando e sem bem vindo em todas as tela

--- js/home.js
+++ js/home.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
     nome = localStorage.getItem('nome')
     elemento_header = document.querySelector('.header')
-    elemento_header.innerHTML = `Bem vinda, ${nome}`
+    elemento_header.innerHTML = `Bem vinda, ${nome}!`
 })
